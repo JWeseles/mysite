576756d6e546cce1e64435b15e5e902b60292d52
JWeseles <jweseles_al@hotmail.com>
2020-12-17 23:12:15 -0300

    reset do mysql

--- societyaabb/settings.py
+++ societyaabb/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'societyaabb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -102,7 +102,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
